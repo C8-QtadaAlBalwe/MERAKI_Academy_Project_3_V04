aa103ba809d90842ebdd05b18b5f5668d678343e
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-09-18 18:00:22 +0300

    authorization

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,9 +1,17 @@
 // This function checks if the user has a permission the passed permission
 
-const authorization = (string) => {
-
-    //TODO: write your code here
+    const authorization = (string) => {
+        return (req, res, next) => {
+          if (!req.token.role.permissions.includes(string)) {
+            return res.status(403).json({
+              success: false,
+              message: `Unauthorized`,
+            });
+          }
+          next();
+        };
+      };
   
-};
+
 
 module.exports = authorization;
